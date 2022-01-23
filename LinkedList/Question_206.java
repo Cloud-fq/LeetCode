460881f07b1e87ff67c62ff3c78da737a42f09e1
Cloud-fq <66189271+Cloud-fq@users.noreply.github.com>
2022-01-23 11:40:34 +0800

    Update Question_206.java

--- LinkedList/Question_206.java
+++ LinkedList/Question_206.java
@@ -1,4 +1,18 @@
 class Solution {
+    //迭代
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur!=null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+    
+    //递归
     public ListNode reverseList(ListNode head) {
         if(head==null||head.next==null) return head;
         ListNode node = reverseList(head.next);
