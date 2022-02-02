8e5207fa3adcd27c93f18d1a0c519b77881fd4d4
Cloud-fq <66189271+Cloud-fq@users.noreply.github.com>
2022-02-02 11:08:11 +0800

    前K个高频元素

---
+++ Stack&Queue/Question_347.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(a) - map.get(b);
+            }
+        });
+        for (int key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.offer(key);
+            } else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove();
+                pq.offer(key);
+            }
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
+        }
+        return ans;
+    }
+}
